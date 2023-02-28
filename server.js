5a70010c7076599bb304bf08415231b7adca48c1
guhansp <spguhansanthanam@gmail.com>
2023-02-28 21:32:14 +0530

    LF setting

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const request = require("request")
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",function(req,res)
-{
-    res.sendFile(__dirname+"/signup.html")
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
 
+var port = 3000;
 
-var port = 3000
-
-app.listen(port,function()
-{
-    console.log("Server is running on Port " + port)
-})
+app.listen(port, function () {
+  console.log("Server is running on Port " + port);
+});
