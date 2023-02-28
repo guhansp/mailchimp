dd8471b21f4a91824b2d34da2ea74ea2368eafff
guhansp <spguhansanthanam@gmail.com>
2023-02-28 21:23:41 +0530

    new

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const request = require("request")
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",function(req,res)
+{
+    res.sendFile(__dirname+"/signup.html")
+})
+
+
+var port = 3000
+
+app.listen(port,function()
+{
+    console.log("Server is running on Port " + port)
+})
